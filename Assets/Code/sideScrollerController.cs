0d5fe231ae7fda6c36502834d54755fa6c91543b
Solokeh <34717870+Solokeh@users.noreply.github.com>
2018-01-23 06:42:54 -0800

    Got basic physics working.

--- Assets/Code/sideScrollerController.cs
+++ Assets/Code/sideScrollerController.cs
@@ -9,6 +9,10 @@ public class sideScrollerController : MonoBehaviour {
     public float skidSmooth = 0.1f;
     public float moveSpeed = 1f;
     public float moveSmooth = 0.1f;
+    public float downForce = 0f;
+    public float distFromGround = 0;
+    public float timeOffGroundVal = 0;
+    public float maxSpeed = 5f;
 
 	void Start ()
     {
@@ -18,6 +22,8 @@ public class sideScrollerController : MonoBehaviour {
 	void FixedUpdate ()
     {
         Movement();
+        CheckGround();
+        LimitVelocity();
 	}
     
     void Rotation()
@@ -33,6 +39,42 @@ public class sideScrollerController : MonoBehaviour {
 
         float angle = (Mathf.Atan2(stepVec.y, stepVec.x) * 180 / Mathf.PI) + 90;
 
-        rb2D.MovePosition(Vector3.Lerp(new Vector2(transform.position.x, transform.position.y), new Vector2(transform.position.x, transform.position.y) + new Vector2(stepVec.x, 0) * moveSpeed, moveSmooth));
+        rb2D.MovePosition(Vector3.Lerp(new Vector2(transform.position.x, transform.position.y), new Vector2(transform.position.x, transform.position.y) + new Vector2(stepVec.x, -downForce) * moveSpeed, moveSmooth));
     }
+
+    void CheckGround()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0, -1.1f, 0), -Vector2.up);
+
+        if(hit.transform.tag == "Ground")
+        {
+            distFromGround = hit.distance;
+        }
+
+        if(distFromGround >= 0.1f)
+        {
+            timeOffGround();
+        }
+        else
+        {
+            downForce = 0;
+            timeOffGroundVal = 0;
+
+        }
+    }
+
+    void LimitVelocity()
+    {
+        if(rb2D.velocity.magnitude >= maxSpeed)
+        {
+            rb2D.velocity = rb2D.velocity.normalized * maxSpeed;
+        }
+    }
+
+    void timeOffGround()
+    {
+        timeOffGroundVal += 0.1f;
+        downForce = timeOffGroundVal;
+    }
+
 }
