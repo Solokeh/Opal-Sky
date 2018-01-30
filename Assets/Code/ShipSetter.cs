b8ada71751e3ca1085fd636a8f9bfed44ef32b72
Ripper53 <Ripper@github.com>
2018-01-30 16:41:42 -0500

    Changes to map generator. Now uses block tiles mixed with platforms.

--- Assets/Code/ShipSetter.cs
+++ Assets/Code/ShipSetter.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class ShipSetter : MonoBehaviour {
     public Transform player;
-    public GameObject platformPrefab;
+    public GameObject platformPrefab, blockPrefab;
     public int sizeX = 10, sizeY = 10;
     public float incrementX = 0.1f, incrementY = 0.1f;
     [Range(0f, 1f)]
     public float threshold = 0.5f;
 
     public void Awake() {
-        ShipGenerator.Generate(player, platformPrefab, sizeX, sizeY, incrementX, incrementY, threshold);
+        ShipGenerator.Generate(player, platformPrefab, blockPrefab, sizeX, sizeY, incrementX, incrementY, threshold);
     }
 }
--- Assets/Code/shipGenerator.cs
+++ Assets/Code/shipGenerator.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class ShipGenerator : MonoBehaviour {
-    public enum Tile { Free, Platform, Entry };
+    public enum Tile { Free, Platform, Block, Entry };
 
-    public static void Generate(Transform player, GameObject platformPrefab, int sizeX, int sizeY, float incrementX = 0.1f, float incrementY = 0.1f, float threshold = 0.5f) {
+    public static void Generate(Transform player, GameObject platformPrefab, GameObject blockPrefab, int sizeX, int sizeY, float incrementX = 0.1f, float incrementY = 0.1f, float threshold = 0.5f) {
         GameObject shipHolder = new GameObject("Ship");
         Tile[,] ship = new Tile[sizeX, sizeY];
         float x = Random.Range(-99999f, 99999f), y = Random.Range(-99999f, 99999f);
         float offsetX = 0f, offsetY = 0f;
-        for (int iY = 0; iY < sizeY; iY++, offsetY += incrementY, offsetX = 0f) {
+        for (int iY = 0; iY < (sizeY - 5); iY++, offsetY += incrementY, offsetX = 0f) {
             for (int iX = 0; iX < sizeX; iX++, offsetX += incrementX) {
                 float randomNum = Mathf.PerlinNoise(x + offsetX, y + offsetY);
                 if (randomNum > threshold) {
@@ -22,17 +22,31 @@ public abstract class ShipGenerator : MonoBehaviour {
         }
         bool entryPlaced = false;
         for (int iX = 0; iX < sizeX; iX++) {
-            for (int iY = (sizeY - 1); iY >= 0; iY--) {
+            for (int iY = (sizeY - 6); iY >= 0; iY--) {
                 if (ship[iX, iY] == Tile.Platform) {
-                    if ((!entryPlaced) && ((iY + 1) < sizeY)) {
-                        ship[iX, iY + 1] = Tile.Entry;
-                        entryPlaced = true;
-                    }
-                    for (int iY2 = (iY - 1); iY2 >= 0; iY2--) {
-                        if (ship[iX, iY2] == Tile.Free) {
-                            break;
+                    if (iY == 0) {
+                        ship[iX, iY] = Tile.Block;
+                    } else {
+                        if ((!entryPlaced) && ((iY + 1) < sizeY)) {
+                            ship[iX, iY + 1] = Tile.Entry;
+                            entryPlaced = true;
+                        }
+                        if (Random.Range(0, 5) == 0) {
+                            for (int iY2 = (iY - 1); iY2 >= 0; iY2--) {
+                                if (ship[iX, iY2] == Tile.Free) {
+                                    break;
+                                } else {
+                                    ship[iX, iY2] = Tile.Free;
+                                }
+                            }
                         } else {
-                            ship[iX, iY2] = Tile.Free;
+                            for (int iY2 = iY; iY2 >= 0; iY2--) {
+                                if (ship[iX, iY2] == Tile.Free) {
+                                    break;
+                                } else {
+                                    ship[iX, iY2] = Tile.Block;
+                                }
+                            }
                         }
                     }
                 }
@@ -42,21 +56,44 @@ public abstract class ShipGenerator : MonoBehaviour {
         float platformSizeX = col.size.x;
         float platformSizeY = col.size.y;
         Vector2 pos = Vector2.zero;
-        for (int iY = 0; iY < sizeY; iY++, pos.y += platformSizeY, pos.x = 0f) {
+        for (int iY = 0; iY < sizeY; iY++, pos.y++, pos.x = 0f) {
             for (int iX = 0; iX < sizeX; iX++, pos.x += platformSizeX) {
                 Tile tile = ship[iX, iY];
                 if (tile == Tile.Platform) {
                     CreatePlatform(platformPrefab, pos, shipHolder.transform);
+                } else if (tile == Tile.Block) {
+                    int blockSizeY = 1;
+                    for (int i = (iY + 1); i < sizeY; i++) {
+                        if (ship[iX, i] == Tile.Block) {
+                            blockSizeY++;
+                            ship[iX, i] = Tile.Free;
+                        } else {
+                            break;
+                        }
+                    }
+                    CreateBlock(blockPrefab, new Vector2(pos.x, pos.y + (blockSizeY / 2f)), new Vector2(platformSizeX, blockSizeY), shipHolder.transform);
                 } else if (tile == Tile.Entry) {
                     PlacePlayer(player, pos);
                 }
             }
         }
+        pos = Vector2.zero;
+        for (int iY = 0; iY < 2; iY++, pos.y += sizeY) {
+            CreateBlock(blockPrefab, new Vector2(((sizeX * platformSizeX) / 2f) - (platformSizeX / 2f), pos.y), new Vector2(sizeX * platformSizeX, 1f), shipHolder.transform);
+        }
+        pos = new Vector2(-0.5f - (platformSizeX / 2f), -1f);
+        for (int iX = 0; iX < 2; iX++, pos.x += (sizeX * platformSizeX) + 1f) {
+            CreateBlock(blockPrefab, new Vector2(pos.x, sizeY / 2f), new Vector2(1f, sizeY + 1f), shipHolder.transform);
+        }
     }
 
     private static void CreatePlatform(GameObject platformPrefab, Vector2 pos, Transform parent) {
-        pos.y /= 2f;
         Instantiate(platformPrefab, pos, Quaternion.identity, parent);
+    }
+
+    private static void CreateBlock(GameObject blockPrefab, Vector2 pos, Vector2 size, Transform parent) {
+        GameObject block = Instantiate(blockPrefab, pos, Quaternion.identity, parent);
+        block.GetComponent<SpriteRenderer>().size = size;
     }
 
     private static void PlacePlayer(Transform player, Vector2 pos) {
